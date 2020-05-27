0be31db1c44c6176b39254cd8215d234ac34371e
ClementFbt <clement.froz@gmail.com>
2020-05-26 21:00:11 -0400

    correct bug in loading json

--- Converters scripts/graphToJson.py
+++ Converters scripts/graphToJson.py
@@ -7,12 +7,26 @@ from random import randrange
 
 data = {"nodes": [], "links": []}
 
-def csvToList(csv):
-    with open(csv) as f:
-        infile = [line for line in f.readlines()]
-        for l in infile:
-            line = l.split()
-            data["nodes"].append({"id": line[0], "name": line[1], "type": randrange(2)})
+def csvToList(classList, classType, classPath):
+    baseUrl = "https://github.com/adempiere/adempiere/tree/develop/base/src/"
+    with open(classList) as f, open(classType) as g, open(classPath) as h:
+        inList = [line.split() for line in f.readlines()]
+        inType = [line.split() for line in g.readlines()]
+        inPath = [line.split(".") for line in h.readlines()]
+        for lineList in inList:
+            for lineType in inType:
+                if lineList[1] == lineType[0]:
+                    for linePath in inPath:
+                        linePath[-1] = linePath[-1].rstrip()
+                        if lineList[1] == linePath[-1]:
+                            data["nodes"].append({
+                                "id": lineList[0],
+                                "name": lineList[1],
+                                "type": lineType[1],
+                                "url": baseUrl + '/'.join(linePath)
+                            })
+                            del(lineType)
+                            del(linePath)
 
 # Convert .txt graph to json graph and add parameters
 def convertToInt(file):
@@ -21,13 +35,13 @@ def convertToInt(file):
         for l in infile:
             line = l.split()
             #TODO add url
-            data["links"].append({"source": line[0], "target": line[1], "value": int(float(line[2])), "url": "TODO"})
+            data["links"].append({"source": line[0], "target": line[1], "value": int(float(line[2])*10)})
     with open('input/graph.json', 'w') as outfile:
         json.dump(data, outfile)
 
 
 def main(argv):
-    csvToList(argv[2])
+    csvToList(argv[2], argv[3], argv[4])
     convertToInt(argv[1])
 
 
